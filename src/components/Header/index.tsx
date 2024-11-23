186be8d812cf096792289c3d19b7599fc2c39a20
Wo0zZ1 <Mper06@mail.ru>
2024-11-23 21:01:34 +0300

    local ip check

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,51 +1,57 @@
-import { FC, useState, useEffect, useRef } from "react";
-import { BiHistory } from "react-icons/bi";
-import { HiHome } from "react-icons/hi";
-import { Link } from "react-router-dom";
-import LastScans from "../LastScans";
+import { FC, useState, useEffect, useRef } from 'react'
+import { BiHistory } from 'react-icons/bi'
+import { HiHome } from 'react-icons/hi'
+import { Link } from 'react-router-dom'
+import LastScans from '../LastScans'
+import YourIp from '../YourIp'
 
 const Header: FC = () => {
-  const [isDropdownVisible, setDropdownVisible] = useState(false);
-  const dropdownRef = useRef<HTMLDivElement>(null);
+	const [isDropdownVisible, setDropdownVisible] = useState(false)
+	const dropdownRef = useRef<HTMLDivElement>(null)
 
-  const toggleDropdown = () => {
-    setDropdownVisible(!isDropdownVisible);
-  };
+	const toggleDropdown = () => {
+		setDropdownVisible(!isDropdownVisible)
+	}
 
-  const handleClickOutside = (event: MouseEvent) => {
-    if (
-      dropdownRef.current &&
-      !dropdownRef.current.contains(event.target as Node)
-    ) {
-      setDropdownVisible(false);
-    }
-  };
+	const handleClickOutside = (event: MouseEvent) => {
+		if (
+			dropdownRef.current &&
+			!dropdownRef.current.contains(event.target as Node)
+		) {
+			setDropdownVisible(false)
+		}
+	}
 
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
+	useEffect(() => {
+		document.addEventListener('mousedown', handleClickOutside)
+		return () => {
+			document.removeEventListener(
+				'mousedown',
+				handleClickOutside,
+			)
+		}
+	}, [])
 
-  return (
-    <header className="sticky w-full text-secondary text-lg h-24 bg-white shadow">
-      <div className="h-full flex items-center justify-between container">
-        <Link to={"/"}>
-          <HiHome size="1.75em" />
-        </Link>
-        <div className="relative flex" ref={dropdownRef}>
-          <BiHistory
-            className="hover:cursor-pointer"
-            size="1.75em"
-            onClick={toggleDropdown}
-          />
-          {isDropdownVisible && <LastScans />}
-        </div>
-      </div>
-    </header>
-  );
-};
+	return (
+		<header className='sticky w-full text-secondary text-lg h-24 bg-white shadow'>
+			<div className='h-full flex items-center justify-between container'>
+				<div className='flex gap-8 items-center'>
+					<Link to={'/'}>
+						<HiHome size='1.75em' />
+					</Link>
+					<YourIp />
+				</div>
+				<div className='relative flex' ref={dropdownRef}>
+					<BiHistory
+						className='hover:cursor-pointer'
+						size='1.75em'
+						onClick={toggleDropdown}
+					/>
+					{isDropdownVisible && <LastScans />}
+				</div>
+			</div>
+		</header>
+	)
+}
 
-export default Header;
-
+export default Header
