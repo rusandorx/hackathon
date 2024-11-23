91722380165bdd1875e93733cc296c88dab1eb5b
Wo0zZ1 <Mper06@mail.ru>
2024-11-24 00:12:24 +0300

    no-cors fix

--- src/components/InputForm/index.tsx
+++ src/components/InputForm/index.tsx
@@ -6,14 +6,14 @@ import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
 interface GetScanFormFields extends HTMLFormControlsCollection {
-  ip?: HTMLInputElement;
-  "ip-start"?: HTMLInputElement;
-  "ip-end"?: HTMLInputElement;
-  "ip-mask"?: HTMLInputElement;
-  "ip-byte"?: HTMLInputElement;
-  "ip-format": HTMLInputElement;
-  ports: HTMLInputElement;
-  "custom-ports"?: HTMLInputElement;
+	ip?: HTMLInputElement
+	'ip-start'?: HTMLInputElement
+	'ip-end'?: HTMLInputElement
+	'ip-mask'?: HTMLInputElement
+	'ip-byte'?: HTMLInputElement
+	'ip-format': HTMLInputElement
+	ports: HTMLInputElement
+	'custom-ports'?: HTMLInputElement
 }
 
 interface GetScanFormElements extends HTMLFormElement {
@@ -21,64 +21,67 @@ interface GetScanFormElements extends HTMLFormElement {
 }
 
 const InputForm: FC = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const scanId = useRef<string | null>(null);
-  const [customPorts, setCustomPorts] = useState(false);
+	const dispatch = useDispatch()
+	const navigate = useNavigate()
+	const scanId = useRef<string | null>(null)
+	const [customPorts, setCustomPorts] = useState(false)
 
-  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setCustomPorts(e.target.value === "custom");
-  };
+	const handleSelectChange = (
+		e: React.ChangeEvent<HTMLSelectElement>,
+	) => {
+		setCustomPorts(e.target.value === 'custom')
+	}
 
-  const handleSubmit = async (e: React.FormEvent<GetScanFormElements>) => {
-    e.preventDefault();
+	const handleSubmit = async (
+		e: React.FormEvent<GetScanFormElements>,
+	) => {
+		e.preventDefault()
 
-    const { elements } = e.currentTarget;
-    const ipFormat = elements["ip-format"].value;
-    const singleFieldIp = elements.ip?.value;
-    const ipStart = elements["ip-start"]?.value;
-    const ipEnd = elements["ip-end"]?.value;
-    const ipMask = elements["ip-mask"]?.value;
-    const ipByte = elements["ip-byte"]?.value;
+		const { elements } = e.currentTarget
+		const ipFormat = elements['ip-format'].value
+		const singleFieldIp = elements.ip?.value
+		const ipStart = elements['ip-start']?.value
+		const ipEnd = elements['ip-end']?.value
+		const ipMask = elements['ip-mask']?.value
+		const ipByte = elements['ip-byte']?.value
 
-    let ip: string;
+		let ip: string
 
-    switch (ipFormat) {
-      case "single":
-      case "multiple": {
-        ip = singleFieldIp!;
-        break;
-      }
-      case "range": {
-        ip = ipStart + "-" + ipEnd;
-        break;
-      }
-      case "cidr": {
-        ip = ipMask + "/" + ipByte;
-        break;
-      }
-      default: {
-        ip = "stfu";
-        break;
-      }
-    }
+		switch (ipFormat) {
+			case 'single':
+			case 'multiple': {
+				ip = singleFieldIp!
+				break
+			}
+			case 'range': {
+				ip = ipStart + '-' + ipEnd
+				break
+			}
+			case 'cidr': {
+				ip = ipMask + '/' + ipByte
+				break
+			}
+			default: {
+				ip = 'stfu'
+				break
+			}
+		}
 
-    const portType = elements.ports.value;
-    const ports = elements["custom-ports"]?.value;
+		const portType = elements.ports.value
+		const ports = elements['custom-ports']?.value
 
-    console.log({ targets: [ip], portType, ports });
+		console.log({ targets: [ip], portType, ports })
 
-    const response = await fetch(
-      `${import.meta.env.VITE_API_BASE_URL ?? ""}/scans/`,
-      {
-        method: "POST",
-        body: JSON.stringify({ targets: [ip], portType, ports }),
-        mode: "no-cors",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      },
-    );
+		const response = await fetch(
+			`${import.meta.env.VITE_API_BASE_URL ?? ''}/scans/`,
+			{
+				method: 'POST',
+				body: JSON.stringify({ targets: [ip], portType, ports }),
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			},
+		)
 
 		dispatch(setScanStatus('loading'))
 		const taskId = (await response.json()).task_id
@@ -129,4 +132,4 @@ const InputForm: FC = () => {
 	)
 }
 
-export default InputForm;
+export default InputForm
