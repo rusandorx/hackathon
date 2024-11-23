62a8085c171765ab0114466f81c545086fdc7a21
rusandorx <ruslanpalkin44@gmail.com>
2024-11-23 19:13:35 +0300

    fix

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,15 @@
-import { FC } from 'react'
+import { FC } from "react";
+import { Link } from "react-router-dom";
 
 const Header: FC = () => {
-	return (
-		<header className='sticky w-full text-secondary text-lg h-24 bg-white shadow'>
-			<div className='h-full flex items-center justify-between container'>
-				{/* TODO Дописать Header */}
-				<div>Logo</div>
-				<div>ogoL</div>
-			</div>
-		</header>
-	)
-}
-export default Header
+  return (
+    <header className="sticky w-full text-secondary text-lg h-24 bg-white shadow">
+      <div className="h-full flex items-center justify-between container">
+        {/* TODO Дописать Header */}
+        <Link to={"/"}>Logo</Link>
+        <div>ogoL</div>
+      </div>
+    </header>
+  );
+};
+export default Header;
