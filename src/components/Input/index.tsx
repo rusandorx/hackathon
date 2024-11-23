8afa7c7c9d7d9ca5818ff7a4462f3f7a0f5826e7
Wo0zZ1 <Mper06@mail.ru>
2024-11-23 17:17:46 +0300

    :(

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,60 +1,63 @@
-import { FC, useCallback, useRef } from "react";
+import { FC, useCallback, useRef } from 'react'
 
-import Typewriter from "typewriter-effect/dist/core";
+import Typewriter from 'typewriter-effect/dist/core'
+import { ipPattern } from '../../utils/regEx'
 
 const inputExamples = [
-  "192.168.1.0",
-  "Google.com",
-  "0.0.0.0-255.255.255.255",
-  // TODO: НЕ ЗАБЫТЬ ПОМЕНЯТЬ
-  "CIDR-нотация",
-];
+	'192.168.1.0',
+	'Google.com',
+	'0.0.0.0-255.255.255.255',
+	'172.31.0.0/16',
+]
 
 const Input: FC = () => {
-  const input = useRef<HTMLInputElement | null>(null);
+	const input = useRef<HTMLInputElement | null>(null)
 
-  const customNodeCreator = useCallback((character: unknown) => {
-    // Add character to input placeholder
-    if (input.current?.placeholder) {
-      input.current.placeholder = input.current.placeholder + character;
-    }
+	const customNodeCreator = useCallback((character: unknown) => {
+		// Add character to input placeholder
+		if (input.current?.placeholder) {
+			input.current.placeholder =
+				input.current.placeholder + character
+		}
 
-    // Return null to skip internal adding of dom node
-    return null;
-  }, []);
+		// Return null to skip internal adding of dom node
+		return null
+	}, [])
 
-  const onRemoveNode = useCallback(() => {
-    if (input.current?.placeholder) {
-      // Remove last character from input placeholder
-      input.current.placeholder = input.current?.placeholder.slice(0, -1);
-    }
-  }, []);
+	const onRemoveNode = useCallback(() => {
+		if (input.current?.placeholder) {
+			// Remove last character from input placeholder
+			input.current.placeholder =
+				input.current?.placeholder.slice(0, -1)
+		}
+	}, [])
 
-  new Typewriter(null, {
-    strings: inputExamples,
-    autoStart: true,
-    loop: true,
-    delay: 75,
-    onCreateTextNode: customNodeCreator,
-    onRemoveNode: onRemoveNode,
-  });
+	new Typewriter(null, {
+		strings: inputExamples,
+		autoStart: true,
+		loop: true,
+		delay: 75,
+		onCreateTextNode: customNodeCreator,
+		onRemoveNode: onRemoveNode,
+	})
 
-  return (
-    <>
-      <label htmlFor="ip-address" className="font-semibold mr-2">
-        Введите IP адрес
-      </label>
-      <input
-        type="text"
-        id="ip-address"
-        ref={input}
-        placeholder=" "
-        name="ip"
-        className="border border-gray-300 focus:outline-none focus:ring-2 focus:ring-primary rounded-md px-3 py-2"
-        required
-      />
-    </>
-  );
-};
+	return (
+		<>
+			<label htmlFor='ip-address' className='font-semibold mr-2'>
+				Введите IP адрес
+			</label>
+			<input
+				type='text'
+				id='ip-address'
+				ref={input}
+				pattern={ipPattern}
+				placeholder=' '
+				name='ip'
+				className='border border-gray-300 focus:outline-none focus:ring-2 focus:ring-primary rounded-md px-3 py-2'
+				required
+			/>
+		</>
+	)
+}
 
-export default Input;
+export default Input
--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,45 +1,4 @@
-import { Variants } from 'framer-motion'
+import { inView, fadeIn } from './animation'
+import { ipRangeRegex, isValidIp } from './regEx'
 
-export type IDirection = 'left' | 'right' | 'up' | 'down' | ''
-
-export const inView = {
-	initial: 'hidden',
-	whileInView: 'show',
-	viewport: { once: true, amount: 0.25 },
-}
-
-export const fadeIn = (
-	direction: IDirection,
-	type: string,
-	delay: number,
-	duration: number,
-): Variants => {
-	return {
-		hidden: {
-			x:
-				direction === 'left'
-					? 100
-					: direction === 'right'
-					? -100
-					: 0,
-			y:
-				direction === 'up'
-					? 100
-					: direction === 'down'
-					? -100
-					: 0,
-			opacity: 0,
-		},
-		show: {
-			x: 0,
-			y: 0,
-			opacity: 1,
-			transition: {
-				type: type,
-				delay: delay,
-				duration: duration,
-				ease: 'easeOut',
-			},
-		},
-	}
-}
+export { inView, fadeIn, ipRangeRegex, isValidIp }
