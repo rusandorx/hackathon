d7d5a81143d359b56fff13a16dfa019e19c526f5
rusandorx <ruslanpalkin44@gmail.com>
2024-11-24 12:52:50 +0300

    pdf

--- src/libs/pdf.tsx
+++ src/libs/pdf.tsx
@@ -1,48 +1,58 @@
 import jsPDF from "jspdf";
-
-import "jspdf-autotable";
-
-// @ts-expect-error idk
-import { saveAs } from "file-saver";
 import { IpData, Port, ScanData } from "../store/slices/scanSlice";
+// @ts-expect-error ffg
+import { saveAs } from "file-saver";
 
 export const createPDF = (scanData: ScanData) => {
   const doc = new jsPDF();
+  let currentY = 10;
 
-  let currentY = 3;
   if (scanData && scanData.ips) {
-    scanData.ips.forEach((ipData: IpData, index: number) => {
-      doc.text(`IP: ${ipData.ip}`, 10, ++currentY * 10);
-      doc.text(`PTR: ${ipData.ptr || "Unknown"}`, 10, ++currentY * 10);
+    scanData.ips.forEach((ipData: IpData) => {
+      doc.text(`IP: ${ipData.ip}`, 10, currentY);
+      currentY += 10;
+      doc.text(`PTR: ${ipData.ptr || "Unknown"}`, 10, currentY);
+      currentY += 10;
 
-      const openPorts = ipData.ports.open.map((port: Port) => [
-        port.port,
-        port.protocol || "N/A",
-        port.service || "N/A",
-        port.version || "N/A",
-        port.vulnerabilities?.map((vul) => vul.title).join(", ") || "None",
-      ]);
+      const openPorts = ipData.ports.open;
 
       if (openPorts.length) {
-        // @ts-expect-error idk
-        doc.autoTable({
-          head: [["Port", "Protocol", "Service", "Version", "Vulnerabilities"]],
-          body: openPorts,
-          startY: ++currentY * 10,
+        // Draw table header
+        doc.text("Port", 10, currentY);
+        doc.text("Protocol", 30, currentY);
+        doc.text("Service", 60, currentY);
+        doc.text("Version", 90, currentY);
+        doc.text("Vulnerabilities", 120, currentY);
+        currentY += 10;
+
+        // Draw table rows
+        openPorts.forEach((port: Port) => {
+          doc.text(port.port.toString(), 10, currentY);
+          doc.text(port.protocol || "N/A", 30, currentY);
+          doc.text(port.service || "N/A", 60, currentY);
+          doc.text(port.version || "N/A", 90, currentY);
+          doc.text(
+            port.vulnerabilities?.map((vul) => vul.title).join(", ") || "None",
+            120,
+            currentY,
+          );
+          currentY += 10;
         });
       }
-      currentY += openPorts.length;
+
       if (ipData.ports.closed.length) {
         doc.text(
           `Closed Ports: ${ipData.ports.closed.join(", ")}`,
           10,
-          ++currentY * 10,
+          currentY,
         );
+        currentY += 10;
       }
-      currentY += 3;
+
+      currentY += 10; // Add some space before the next IP
     });
   } else {
-    doc.text("No data", 10, 10);
+    doc.text("No data", 10, currentY);
   }
 
   return doc;
